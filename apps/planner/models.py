6c32aa1dbaa274958420cf9eff025016d797feeb
vasile.corsunji <vasili.cursunji@gmail.com>
2022-04-19 12:28:29 +0300

    removed unused imports

--- apps/planner/models.py
+++ apps/planner/models.py
@@ -1,10 +1,8 @@
 from datetime import datetime, timedelta
 
-from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import Sum
-from django.utils.html import format_html
 
 from apps.planner.validators import WeekDayValidator
 
